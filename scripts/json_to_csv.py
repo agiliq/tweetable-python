71b5f16c59b7b829dc238d25b880c3a4fa3906b0
Yogesh Kumar <yk.301189@gmail.com>
2018-07-30 12:01:00 +0530

    Fixed scripts to pass pytest

--- scripts/json_to_csv.py
+++ scripts/json_to_csv.py
@@ -3,4 +3,4 @@ import json, csv
 
 _json = json.loads(open('data/example.json', 'r').read())
 out = open('data/converted.csv', 'w')
-[out.write(','.join([x[x.keys()[i]] for i in range(len(x))]) + '\n') for x in _json]
+[out.write(','.join([x[list(x.keys())[i]] for i in range(len(x))]) + '\n') for x in _json]
